f39ecd6d9863411d74e2253b9e61c99f0021cf1f
Sai Datta Kunchala <saidatta1432015@gmail.com>
2025-02-26 18:51:10 +0530

    Datta | Update server.js file

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const APP_NAME = process.env.APP_NAME;
+
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log('Request served from node');
+    console.log(`Request served from ${APP_NAME}`);
 })
 
 app.listen(port, ()=> {
-    console.log(`node is listening on port ${port}`);
+    console.log(`${APP_NAME} is listening on port ${port}`);
 })
